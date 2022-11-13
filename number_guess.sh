385a9f160a3164c33b768461aa341c3d1392dfe7
Sam <96598232+SamWalts@users.noreply.github.com>
2022-11-13 13:59:32 -0600

    updated
    Fixed bug with USER_ID not being added to new player, line 19.
    Used sed to remove white spaces from $BEST_GAME and $GAMES_PLAYED.

--- number_guess.sh
+++ number_guess.sh
@@ -14,8 +14,9 @@ function CHECK_USERNAME() {
 CHECK_USER=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 if [[ -z $CHECK_USER ]]
 then
-  echo -e "Welcome,$USERNAME! It looks like this is your first time here."
+  echo -e "Welcome, $USERNAME! It looks like this is your first time here."
 ADD_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME');")
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 else
 #need user_id
   USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
@@ -23,13 +24,14 @@ else
   GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games WHERE user_id = $USER_ID";)
 # (min) guesses for games played
 BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id = $USER_ID";)
-echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+echo -e "\nWelcome back, $USERNAME! You have played $(echo $GAMES_PLAYED | sed -E 's/^ *| *$//g') games, and your best game took $(echo $BEST_GAME | sed -E 's/^ *| *$//g') guesses."
 fi
 GAME $USERNAME $GAMES_PLAYED $USER_ID $BESTGAME
 }
 
 #function to generate random numbers 1-50
 GAME() {
+
   NUMBER_TO_GUESS=$(( $RANDOM % 1000 + 1 ))
   echo -e "\nGuess the secret number between 1 and 1000:"
   #while loop
@@ -57,15 +59,14 @@ GAME() {
     echo -e "\nIt's higher than that, guess again:"
   continue
   fi
+  
   #Congratulations and exit loop
-  echo -e "\nYou guessed it in $GUESSES tries. The secret number was $NUMBER_TO_GUESS. Nice job!"
-
+  echo -e "\nYou guessed it in $GUESSES tries. $USER_ID The secret number was $NUMBER_TO_GUESS. Nice job!"
   #add to games table
+
   INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(guesses, user_id) VALUES($GUESSES, $USER_ID)")
-break
+  break
 done
-
-
 }
 
 LOGIN
